100a44a37d30c984b204425eb82b10397dd98dc1
solostyle <spam@solostyle.net>
2012-02-05 19:37:57 -0600

    sort of got footer. probably need to put blocks in a list and display them inline. it doesn't make sense

--- app/views/footer.php
+++ app/views/footer.php
@@ -3,19 +3,19 @@
 
 <div id="footer-container">
 	<footer class="wrapper">
-		<div class="block">
+		<div class="footer-block">
 			<!-- <p><img src="<?php echo BASE_PATH.DS.'img/me.jpg'?>" /></p> -->
 			<p style="text-align:center"><img src="<?php echo BASE_PATH.DS.'img/me_blurry.jpg'?>" height="240px" /></p>
 		</div>
-		<div class="block">
+		<div class="footer-block">
 			<h2>About</h2>
-			<p>I'm Archana. This is my blog about Learn HTML5, JavaScript (jQuery included), CSS3, and PHP-based MVC frameworks. As I update my knowledge of new web technology I will share my discoveries and get excited about things.</p>
+			<p>I'm Archana. This is my blog about HTML5, JavaScript (jQuery included), CSS3, and PHP-based MVC frameworks. As I update my knowledge of new web technology I will share my discoveries and get excited about things.</p>
 		</div>
-		<div class="block hidden">
+		<div class="footer-block hidden">
 			<h2>Recent</h2>
 			
 		</div>
-		<div class="block">
+		<div class="footer-block">
 			<h2>Feeds</h2>
 			<p>meditations: <a href="<?php echo BASE_PATH.DS.'rss.xml'?>">RSS</a> | <a href="<?php echo BASE_PATH.DS.'atom.xml'?>">Atom</a></p>
 			<p>last.fm: <a href="http://ws.audioscrobbler.com/1.0/user/solostyle/recenttracks.rss">RSS</a></p>
